10792e5ad4c4159c8666c09996df61eedfbc228f
Misha <pochitalin.m@gmail.com>
2023-06-28 13:21:14 +0200

    Tasks-1: testnet config added

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,5 +4,10 @@ module.exports = {
   solidity: "0.8.17",
   paths: {
     artifacts: "./app/src/artifacts",
-  }
+  },
+  networks: {
+    goerli: {
+      url: process.env.TESTNET_RPC_URL
+    },
+  },
 };
